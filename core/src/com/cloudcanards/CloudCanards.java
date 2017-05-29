c866df23c22cede8c137c4a14822a23995e01fcb
creativitRy <creativitRy@gmail.com>
2017-05-29 12:04:12 -0500

    removed comment

--- core/src/com/cloudcanards/CloudCanards.java
+++ core/src/com/cloudcanards/CloudCanards.java
@@ -1,13 +1,16 @@
 package com.cloudcanards;
 
-//its-mee-kimchee
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * CloudCanards
+ *
+ * @author creativitRy
+ */
 public class CloudCanards extends ApplicationAdapter
 {
 	SpriteBatch batch;
